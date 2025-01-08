6cf6cbbffcb9e69053868cc8c6a6ee80ddb310a4
Evert Heylen <evertheylen@gmail.com>
2025-01-08 18:07:24 +0100

    Now with persistent containers and simpler

--- arch-with-code-server/setup-code-server.sh
+++ arch-with-code-server/setup-code-server.sh
@@ -2,17 +2,38 @@
 
 set -eu
 
-if [[ ! $1 =~ ^[0-9]+$ ]]; then
-  echo "Error: First argument is not a number." >&2
-  exit 1
-fi
+# if [ "$(id -u)" -eq 0 ]; then
+#     su - evert -c "bash $0"
+#     exit
+# fi
 
-systemctl enable --user code-server
+while true; do
+    read -p "If you want to setup code-server, enter which port or type N: " response
 
-mkdir -p ~/.config/code-server
+    if [[ "$response" =~ ^[0-9]+$ ]] && [ "$response" -ge 1 ] && [ "$response" -le 65535 ]; then
+        port=$response
+        break
+    elif [[ "$response" =~ ^n|N ]]; then
+        echo "No code-server."
+        echo "[]" > /extra_podman_options.json
+        exit
+    else
+        echo "Invalid port number. Please enter a number between 1 and 65535."
+    fi
+done
 
-cat >~/.config/code-server/config.yaml <<EOL
-bind-addr: 0.0.0.0:$1
+# also needs linger enabled for user in systemd
+ID=$(id -u evert)
+sudo -u evert bash -c "XDG_RUNTIME_DIR=/run/user/$ID DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/$ID/bus systemctl --user enable code-server"
+
+mkdir -p /home/evert/.config/code-server
+
+cat >/home/evert/.config/code-server/config.yaml <<EOL
+bind-addr: 0.0.0.0:$port
 auth: none
 cert: false
 EOL
+
+chown evert:evert /home/evert/.config/code-server/config.yaml
+
+echo "[\"--publish\", \"$port:$port\"]" > /extra_podman_options.json
--- probox.py
+++ probox.py
@@ -8,8 +8,11 @@ import json
 from dataclasses import dataclass, asdict
 import subprocess
 import random
+import tempfile
 
-boxes_dir = Path("/var/Bestanden/Boxes")
+YELLOW = '\033[1;33m'
+END = '\033[0m'
+GENERIC_NAMES = {'src', 'source', 'project', 'dir', 'folder', 'git', 'repo', 'repository'}
 
 
 def capture_podman(*args):
@@ -17,179 +20,124 @@ def capture_podman(*args):
     return json.loads(res.stdout)
 
 
-def run_podman(*args, check=True, **kwargs):
+def run_podman(*args, check=True, quiet=False, **kwargs):
     command = ['podman', *args]
-    print(">>> ", ' '.join(command))
-    return subprocess.run(command, check=check, **kwargs)
+    print(f"{YELLOW}>>> {' '.join(command)}{END}")
+    if quiet:
+        kwargs['stdout'] = subprocess.DEVNULL
+    return subprocess.run(command, check=check, text=True, **kwargs)
 
 
-# Only the ones with a name (TODO versioned local images?)
-def reset():
-    global probox_images, probox_images_by_path, probox_images_by_boxname, box_names
-    probox_images = [im for im in capture_podman('images', '--filter', 'label=probox.project_path') if im.get('Names')]
-
-    probox_images_by_path = {
-        Path(im['Labels']['probox.project_path']).absolute(): im
-        for im in probox_images
-    }
-    probox_images_by_boxname = {
-        im['Labels']['probox.name']: im
-        for im in probox_images
-    }
-
-    box_names = [box_dir.name for box_dir in boxes_dir.iterdir()]
+def get_containers():
+    containers = capture_podman('container', 'ls', '--all', '--filter', 'label=probox.project_path')
+    containers_by_path = {Path(c['Labels']['probox.project_path']): c for c in containers}
+    containers_by_name = {c['Names'][0]: c for c in containers}
+    return containers_by_path, containers_by_name
 
 
-reset()
+def suggest_name(path, _taken):
+    taken = set(_taken) | GENERIC_NAMES
 
-
-def suggest_name(path):
+    # First try: pick the name of the folder we're in
     abspath = path.absolute()
-    if abspath.name not in box_names:
+    if abspath.name not in taken:
         return path.name
 
-    # boxes_at_higher_dir = set(abspath.parents) & probox_images_by_path.keys()
+    # Second try: use the parent name + current dir (e.g. if we're making a lot of 'src' dirs)
+    if abspath.parent.name != '':
+        combo = abspath.parent.name + '-' + abspath.name
+        if combo not in taken:
+            return combo
 
-    # if len(boxes_at_higher_dir) == 0:
-    # combo = abspath.parent.name + '-' + abspath.name
-    # if combo not in box_names:
-    #     return combo
-
+    # Third try: use a number
     i = 2
     while True:
         name_and_number = abspath.name + f"-{i:>03}"
-        if name_and_number not in box_names:
+        if name_and_number not in taken:
             return name_and_number
         i += 1
 
 
 def create(args):
+    containers_by_path, containers_by_name = get_containers()
     path = Path(os.getcwd() if args.path is None else args.path).absolute()
-    if path in probox_images_by_path:
-        print("Path already registered!", probox_images_by_path[path]['Names'][0])
+    if path in containers_by_path:
+        print("Path already registered!", containers_by_path[path]['Names'][0])
         sys.exit(1)
 
-    name = args.name or suggest_name(path)
+    name = args.name or suggest_name(path, containers_by_name.keys())
     if name is None:
         print("Couldn't determine a name from the path. Specify a name with --name.")
         sys.exit(1)
-    if name in box_names:
-        print(f"Name '{name}' is already taken.")
-        sys.exit(1)
 
-    from_name_or_path = getattr(args, 'from') or 'base'
+    basic_create_options = ['--name', name, '--hostname', name]
 
-    if '/' in from_name_or_path:
-        from_image_path = Path(from_name_or_path).absolute()
-        # No parent dir logic here
-        image = probox_images_by_path.get(str(from_image_path))
-        if image is not None:
-            from_image_name = image["Names"][0]
-    elif from_name_or_path in box_names:
-        from_image_name = f"localhost/{from_name_or_path}:latest"
+    from_image = getattr(args, 'from') or 'arch-with-code-server'
+    image_data = capture_podman('image', 'inspect', from_image)[0]
+    post_create_cmd = image_data["Config"]["Labels"].get("probox.post_create")
 
-    box_dir = boxes_dir / name
-    box_dir.mkdir()
+    if post_create_cmd:
+        run_podman('create', *basic_create_options, from_image, quiet=True)
+        try:
+            run_podman('start', name, quiet=True)
+            run_podman('exec', '-it', name, post_create_cmd, check=True)
+            # Why all this work? See https://github.com/containers/podman/issues/18309
+            res = run_podman('commit', name, '--pause=true', capture_output=True)
+            # Next command will recreate it
+            from_image = res.stdout.strip()
 
-    labels = {
-        'project_path': path,
-        'name': name,
-    }
+            with tempfile.TemporaryDirectory() as tempdir:
+                extra_options_filename = Path(tempdir) / 'extra_podman_options.json'
+                cp_res = run_podman('cp', f'{name}:/extra_podman_options.json', str(extra_options_filename), check=False)
+                if cp_res.returncode == 0:
+                    with open(extra_options_filename) as f:
+                        extra_podman_options = json.load(f)
+        finally:
+            run_podman('stop', name, quiet=True)
+            run_podman('rm', name, quiet=True)
 
-    # write Containerfile
-    containerfile_path = box_dir / 'Containerfile'
-    with open(containerfile_path, 'w') as f:
-        f.write(f"FROM {from_image_name}\n")
+    else:
+        extra_podman_options = []
 
-        for label, value in labels.items():
-            f.write(f"LABEL probox.{label} {str(value)}\n")
+    # TODO get rid of --security-opt label=disable ???
+    # how does toolbx do it? the example given in docs specifically mentions "mounting entire home directory"
+    # https://docs.podman.io/en/latest/markdown/podman-run.1.html#volume-v-source-volume-host-dir-container-dir-options
 
-    print(f">>> Wrote file://{containerfile_path}")
-
-    run_podman('build', str(boxes_dir / name), '-t', name.lower())
-
-
-# build
-# podman build . -t arch-base
-
-# run with port mappings
-# podman run -p 8000-9000:8000-9000 -i -t arch-base
-
-# volume?
-# podman volume create --driver local --opt type=none --opt device=/path/to/host-dir --opt o=bind FOO
-
-# run shell in running container
-# podman exec -i -t --user evert --env XDG_RUNTIME_DIR=/run/user/1000 --env DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus --workdir /home/evert dreamy_knuth
+    run_podman(
+        'create', *basic_create_options, '--label', f'probox.project_path={path}',
+        '--userns=keep-id', '--security-opt', 'label=disable',
+        '--volume', f'{path}:{path}',
+        *extra_podman_options,
+        from_image
+    )
 
 
-def find_box_name_by_path_or_name(path_or_name):
+def find_container_name_by_path_or_name(containers_by_path, containers_by_name, path_or_name):
     if path_or_name is None or '/' in path_or_name:
         deep_path = Path(path_or_name or os.getcwd()).absolute()
-        print("Searching", deep_path, 'in', probox_images_by_path.keys())
         for path in [deep_path] + list(deep_path.parents):
-            image = probox_images_by_path.get(path
-            if image is not None:
-                return image["Labels"]['probox.name']
+            con = containers_by_path.get(path)
+            if con is not None:
+                return con["Names"][0]
+        print("No container found for directory", deep_path, 'in', containers_by_path.keys())
+        sys.exit(1)
 
-    if path_or_name not in box_names:
+    if path_or_name not in containers_by_name:
         print(f"Couldn't find name '{path_or_name}'")
         sys.exit(1)
 
-
-def get_ports(image):
-    return {
-        label[len('probox.ports.'):]: int(val)
-        for label, val in image['Labels'].items() if label.startswith('probox.ports.')
-    }
+    return path_or_name
 
 
 def run(args):
-    box_name = find_box_name_by_path_or_name(args.path_or_name)
-    print(f">>> Found box '{box_name}'")
-    image = probox_images_by_boxname[box_name]
-    box_path = boxes_dir / box_name
-    project_path = Path(image['Labels']['probox.project_path'])
+    containers_by_path, containers_by_name = get_containers()
+    container_name = find_container_name_by_path_or_name(containers_by_path, containers_by_name, args.path_or_name)
 
-    # check if exists
-    containers = capture_podman('ps', '--filter', f'label=probox.name={box_name}')
+    container_data = capture_podman('container', 'inspect', container_name)[0]
+    project_path = Path(container_data['Config']['Labels']['probox.project_path'])
 
-    if len(running) == 0:
-        # build image
-        run_podman('build', str(boxes_dir / box_name), '-t', box_name.lower())
-        reset()
-
-        # # check if a volume exists
-        # existing_volumes = capture_podman('volume', 'ls', '--filter', f'label=probox.name={box_name}')
-        #
-        # if len(existing_volumes) == 0:
-        #     volume_name = f'{box_name}-{random.randint(0, 0xFFFF):04x}'
-        #     # TODO put volume in Boxes??
-        #     # ... '--driver', 'local', '--opt', f'device={str(box_path / '_root')}',
-        #     run_podman('volume', 'create', volume_name, '--label', f"probox.name={box_name}")
-        # elif len(existing_volumes) == 1:
-        #     volume_name = existing_volumes[0]["Name"]
-        # else:
-        #     print("Found multiple volumes for this box")
-        #     sys.exit(1)
-        # '--volume', f'{volume_name}:/home', ???
-
-        # run in background
-        ports = []
-        for p in get_ports(image).values():
-            print("PORT", p)
-            ports.extend(['--publish', f'{p}:{p}'])
-
-        running_name = f"{box_name}-{random.randint(0, 0xFFFF):04x}"
-
-        # TODO get rid of --security-opt label=disable ???
-        # how does toolbx do it? the example given in docs specifically mentions "mounting entire home directory"
-        # https://docs.podman.io/en/latest/markdown/podman-run.1.html#volume-v-source-volume-host-dir-container-dir-options
-        run_podman('run', '--userns=keep-id', *ports, '--security-opt', 'label=disable', '--volume', f'{project_path}:{project_path}', '--name', running_name, '--hostname', running_name, '--detach', image['Names'][0])
-    elif len(running) > 1:
-        print('Weird: more than one container running: ', ', '.join(r['Names'][0] for r in running))
-        sys.exit(1)
-    else:
-        running_name = running[0]['Names'][0]
+    if not container_data['State']['Running']:
+        run_podman('start', container_name, quiet=True)
 
     cwd = Path(os.getcwd()).absolute()
     if project_path == cwd or project_path in cwd.parents:
@@ -197,18 +145,25 @@ def run(args):
     else:
         workdir = Path('/home/evert')
 
-    run_podman('exec', '-it', '--user', 'evert', '--workdir', str(workdir), '--env', 'XDG_RUNTIME_DIR=/run/user/1000', '--env', 'DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus', running_name, '/bin/zsh', check=False)
-
-
-def ps(args):
-    run_podman('ps', '--filter', 'label=probox.name')
+    run_podman('exec', '-it', '--user', 'evert', '--workdir', str(workdir), '--env', 'XDG_RUNTIME_DIR=/run/user/1000', '--env', 'DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus', container_name, '/bin/zsh', check=False)
 
 
 def stop(args):
-    box_name = find_box_name_by_path_or_name(args.path_or_name)
-    running = capture_podman('ps', '--filter', f'label=probox.name={box_name}')
-    for r in running:
-        run_podman('stop', r['Names'][0])
+    containers_by_path, containers_by_name = get_containers()
+    container_name = find_container_name_by_path_or_name(containers_by_path, containers_by_name, args.path_or_name)
+
+    container_data = capture_podman('container', 'inspect', container_name)[0]
+    if container_data['State']['Running']:
+        run_podman('stop', container_name)
+
+
+def name(args):
+    containers_by_path, containers_by_name = get_containers()
+    print(find_container_name_by_path_or_name(containers_by_path, containers_by_name, args.path_or_name))
+
+
+def ls(args):
+    run_podman('ps', '--all', '--size', '--filter', 'label=probox.project_path', '--format', 'table {{.ID}} {{.Size}} {{.Status}} {{.Ports}} {{.Names}} {{.Mounts}}')
 
 
 def main():
@@ -220,22 +175,27 @@ def main():
     create_parser = subparsers.add_parser('create', help="Create a new container (box) for your project")
     create_parser.add_argument('path', nargs='?', default=None, help="Path to attach to container (default = working dir)")
     create_parser.add_argument('--name', help="Set the name for the container")
-    create_parser.add_argument('--from', help="Container image to base this one upon (path or name)")
+    create_parser.add_argument('--from', help="Container image to base this one upon")
     create_parser.set_defaults(func=create)
 
     # 'run' command
-    run_parser = subparsers.add_parser('run', help="Run an existing container")
-    run_parser.add_argument('path_or_name', nargs='?', default=None, help="Path or name of container to run (default = working dir)")
+    run_parser = subparsers.add_parser('run', help="Run an existing container (start and exec)")
+    run_parser.add_argument('path_or_name', nargs='?', default=None, help="Path or name of container (default = working dir)")
     run_parser.set_defaults(func=run)
-
-    # 'ps' command
-    ps_parser = subparsers.add_parser('ps', help="List all containers")
-    ps_parser.set_defaults(func=ps)
 
     # 'stop' command
     stop_parser = subparsers.add_parser('stop', help="Stop a container")
-    stop_parser.add_argument('path_or_name', nargs='?', default=None, help="Path or name of container to stop")
+    stop_parser.add_argument('path_or_name', nargs='?', default=None, help="Path or name of container (default = working dir)")
     stop_parser.set_defaults(func=stop)
+
+    # 'name' command
+    name_parser = subparsers.add_parser('name', help="Get name of container attached to directory")
+    name_parser.add_argument('path_or_name', nargs='?', default=None, help="Path or name of container (default = working dir)")
+    name_parser.set_defaults(func=name)
+
+    # 'ls' command
+    ls_parser = subparsers.add_parser('ls', help="List all probox containers")
+    ls_parser.set_defaults(func=ls)
 
     # Parse and run the appropriate function, or show help
     args = parser.parse_args()
@@ -243,7 +203,6 @@ def main():
         parser.print_help()
         return
     args.func(args)
-
 
 
 if __name__ == "__main__":
